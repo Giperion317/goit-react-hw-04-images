4a1523ac6573c362561a900ab5f5f8e248cefb35
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-12-12 05:45:42 +0100

    Update goit-react-hw-04-images

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,21 +3,20 @@ import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Backdrop, ImageModal, CloseBtn } from './Modal.styled';
 
-const modalroot = document.querySelector('#modal-root')
+const modalroot = document.querySelector('#modal-root');
 
-export const Modal = ({image: { src, alt }, closeModal}) => {
+export const Modal = ({ image: { src, alt }, closeModal }) => {
   useEffect(() => {
+    const closeByEsc = event => {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    };
     window.addEventListener('keydown', closeByEsc);
     return () => {
       window.removeEventListener('keydown', closeByEsc);
     };
-  }, [])
-
-  const closeByEsc = (event) => {
-    if (event.code === 'Escape') {
-      closeModal();
-    }
-  };
+  }, [closeModal]);
 
   const closeByBackDrop = event => {
     if (event.target === event.currentTarget) {
@@ -26,15 +25,15 @@ export const Modal = ({image: { src, alt }, closeModal}) => {
   };
 
   return createPortal(
-      <Backdrop onClick={closeByBackDrop}>
-        <ImageModal>
-          <CloseBtn type="button" onClick={() => closeModal()}></CloseBtn>
-          <img src={src} alt={alt} />
-        </ImageModal>
-      </Backdrop>,
-      modalroot
-    );
-}
+    <Backdrop onClick={closeByBackDrop}>
+      <ImageModal>
+        <CloseBtn type="button" onClick={() => closeModal()}></CloseBtn>
+        <img src={src} alt={alt} />
+      </ImageModal>
+    </Backdrop>,
+    modalroot
+  );
+};
 
 Modal.propTypes = {
   image: PropTypes.object.isRequired,
